015a9dbecdbba10f24d507b862e59f30999703a4
Skye <ollie@ollierobinson.co.uk>
2023-07-25 21:32:01 +0100

    begin implementing basic framework

---
+++ src/Sandy.Framework/SandyApp.cs
@@ -0,0 +1,13 @@
+﻿using Pie.Windowing;
+using Sandy.Graphics;
+
+namespace Sandy.Framework;
+
+public abstract class SandyApp : IDisposable
+{
+    public Window Window;
+
+    public Renderer Renderer;
+    
+    public static SandyApp Instance { get; private set; }
+}
